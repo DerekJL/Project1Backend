e4435fd09fcfbcbc45a10bbbd3e3aef0b3b2dc5b
john.beineke <john.beineke@gmail.com>
2018-08-01 16:34:48 -0400

    update

--- src/main/java/com/revature/repository/WorkoutRepository.java
+++ src/main/java/com/revature/repository/WorkoutRepository.java
@@ -12,7 +12,7 @@ public interface WorkoutRepository {
 	public List<Workout> getAllWorkouts();
 	public List<Workout> getWorkoutsByVisibility(int visibilityId);
 	public Workout getWorkoutById(int id);
-	public List<Workout> getWorkoutsByUserId(Workout wk);	
+	public List<Workout> getWorkoutsByUserId(int id);	
 //	public Workout getWorkoutByType(Workout wk);
 //	public Workout getWorkoutByName(Workout wk);
 	public Workout createWorkout(Workout new_wk); 
